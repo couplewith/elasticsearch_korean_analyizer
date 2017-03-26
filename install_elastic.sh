76f56dec8ecddf849c41d8842ec688b22da7b680
couplewith <couplewith@gmail.com>
2017-03-26 16:51:34 +0900

    Update install_elastic.sh

--- install_elastic.sh
+++ install_elastic.sh
@@ -1,1 +1,18 @@
- yum install elasticseach
+
+
+
+yum install elasticseach
+
+# RPM으로 설치 하기 전에 
+# /etc/yum.repos.d/elasticsearch.repo
+# [elasticsearch-5.x]
+# name=Elasticsearch repository for 5.x packages
+# baseurl=https://artifacts.elastic.co/packages/5.x/yum
+# gpgcheck=1
+# gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+# enabled=1
+# autorefresh=1
+# type=rpm-md
+
+
+
